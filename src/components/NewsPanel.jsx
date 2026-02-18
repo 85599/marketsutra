239eddb4ff235f71d5d8ad7e796ffd2381566a88
Callmejainsahab_ <khushaljain023@gmail.com>
2026-02-18 14:03:33 +0530

    Create NewsPanel component for displaying live news

---
+++ src/components/NewsPanel.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const NewsPanel = () => {
+    return (
+        <div className="news-panel">
+            <h2>Live News Updates</h2>
+            <ul>
+                {/* News items will be displayed here */}
+                <li>Sample news item 1</li>
+                <li>Sample news item 2</li>
+                <li>Sample news item 3</li>
+            </ul>
+        </div>
+    );
+};
+
+export default NewsPanel;
